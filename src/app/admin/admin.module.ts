4a54fe86b9d5ecc7c3a2c5ca20d7599b4608d90f
Luis Alberto Covarrubias González <lcovarrubias@outlook.com>
2022-08-17 16:52:32 -0500

    adding files to firebase storage

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,12 +10,10 @@ import { DashComponent } from './components/dash/dash.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { FormProductComponent } from './components/form-product/form-product.component';
-import { LoginComponent } from './components/login/login/login.component';
-import { UserRecordComponent } from './components/user-record/user-record.component';
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, TableComponent, DashComponent, ProductsListComponent, OrdersComponent, FormProductComponent, LoginComponent, UserRecordComponent, ProductEditComponent],
+  declarations: [ProductFormComponent, NavComponent, TableComponent, DashComponent, ProductsListComponent, OrdersComponent, FormProductComponent, ProductEditComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
--- src/app/auth/components/user-record/user-record.component.ts
+++ src/app/auth/components/user-record/user-record.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-user-record',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRecordComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) { 
+    this.buildForm();
+  }
 
   ngOnInit(): void {
   }
 
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required]],
+      password: ['', [
+        Validators.required, 
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}'), 
+      ]],
+    })
+  }
+
+  get emailField(){
+    return this.form.get('email');
+  }
+
+  get passwordField(){
+    return this.form.get('password');
+  }
+
+  get nameField(){
+    return this.form.get('name');
+  }
+
+  createUser(event: Event){
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      this.authService.createUser(value.email, value.password)
+      .then(() => {
+        this.router.navigate(['./auth/login']);
+      });
+      console.log('The user was create with success!');  
+    }
+  }
+
 }
+
